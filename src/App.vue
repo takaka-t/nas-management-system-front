138ecc850325079ee569e808f8f3607334edb6c1
takaka-t <>
2023-12-30 16:14:54 +0900

    基本のレイアウト実装

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { ref } from "vue";
+import { RouterView } from "vue-router";
+import { mdiMagnify, mdiCog } from "@mdi/js";
+import Top from "@/views/Top.vue";
+import Search from "@/views/Search.vue";
+import Admin from "@/views/Admin.vue";
+
+const isShowDilogSearch = ref(false);
+const isShowDilogAdmin = ref(false);
 </script>
 
 <template>
-  <header>
-    <div>
-      <nav>
-        <RouterLink to="/">top</RouterLink>
-        <RouterLink to="/search">search</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <v-layout>
+    <v-app-bar color="primary">
+      <v-toolbar-title>nas管理くん</v-toolbar-title>
+      <v-btn variant="elevated" :prepend-icon="mdiMagnify" @click.stop="isShowDilogSearch = true">検索</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn variant="outlined" :prepend-icon="mdiCog" @click.stop="isShowDilogAdmin = true">管理</v-btn>
+    </v-app-bar>
 
-  <RouterView />
+    <v-main>
+      <v-container>
+        <Top />
+      </v-container>
+
+      <v-dialog v-model="isShowDilogSearch" persistent>
+        <Search />
+      </v-dialog>
+
+      <v-dialog v-model="isShowDilogAdmin" persistent>
+        <Admin />
+      </v-dialog>
+    </v-main>
+  </v-layout>
 </template>
 
 <style></style>
